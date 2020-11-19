19def173bdc4cae66a4c537ae9376edf496d6953
Hirokazu MORIKAWA <morikw2@gmail.com>
2020-11-19 16:25:26 +0900

    update config

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 
 git pull
 ./scripts/feeds update -a
-./scripts/feeds install -a
+./scripts/feeds install -a -f
 
 # If you do not want to change the settings, please comment out.
 cp gl-mt300n-v2.diffconfig .config
