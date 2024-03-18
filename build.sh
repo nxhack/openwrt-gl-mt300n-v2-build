3cc51111faeda87a6c5bd00acc09f5ed4d8e33a7
Hirokazu MORIKAWA <morikw2@gmail.com>
2024-03-18 15:31:11 +0900

    build.sh: more sleep

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ git clone --depth 1 https://git.openwrt.org/feed/routing.git
 git clone --depth 1 https://git.openwrt.org/project/luci.git
 ## FIXME: RPC failed; curl 18 transfer closed with outstanding read data remaining
 git clone --depth 1 https://git.openwrt.org/feed/packages.git || true
-sleep 10
+sleep 60
 git clone --depth 1 https://git.openwrt.org/feed/packages.git || true
 cd ..
 ./scripts/feeds update -a
